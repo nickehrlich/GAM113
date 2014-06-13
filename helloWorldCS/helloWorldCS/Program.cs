5d9a6602684e8b24015571190b0e71876866925c
nickehrlich <nick.ehrlich@live.com>
2014-06-13 09:30:11 -0500

    commit helloWorld for review

---
+++ helloWorldCS/helloWorldCS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello... Doctor... Name... Continue... Yesterday... Tomorrow...\n\nGood morning, Dr. Chandra. This is HAL. I'm ready for my first lesson.");
+            Console.Read();
+
+            //Console.WriteLine("Hello... Doctor... Name... Continue... Yesterday... Tomorrow...\n\nGood morning, Dr. Chandra. This is HAL. I'm ready for my first lesson.");
+            //Console.ReadLine();
+
+            /*
+             * HTTP Reference
+             * 
+             * Console.Write Method: http://msdn.microsoft.com/en-us/library/system.console.write(v=vs.110).aspx
+             * Console.WriteLine Method: http://msdn.microsoft.com/en-us/library/system.console.writeline(v=vs.110).aspx
+             * 
+             */
+        }
+    }
+}
